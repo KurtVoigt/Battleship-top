416648d5f5ce90bcaadf6496d085dc62933e099e
KurtVoigt <kjvoigtritter@gmail.com>
2024-01-02 15:38:44 +0100

    ai ship placement algo rough draft done, tests must be written for it

--- src/classes/player.ts
+++ src/classes/player.ts
@@ -1,5 +1,5 @@
 import { Gameboard } from "./gameboard";
-import type { coords } from "./gameboard";
+import type { coords, shipPlacementType } from "./gameboard";
 class Player {
     private board: Gameboard
     constructor(board: Gameboard) {
@@ -10,9 +10,37 @@ class Player {
         this.board.receiveAttack(getRandomCoord());
     }
 
+    setAIShips(){
+        const shiparr = [5,4,3,3,2];
+        const orientarr: Array<"h" | "v"> = ["h", "v"];
+        for(let i = 0; i < shiparr.length; i++){
+            const orient = orientarr[Math.floor(Math.random()*2)];
+            this.placeAIShips(shiparr[i],orient)
+        }
+    }
+    
+    //need an algorithm that will place ships in a random manner
+    //look up ways to do this in a less random fashion.
+    private placeAIShips(length:number, orient:"h"|"v"):void {
+        while(true){//keep searching until ship is placed?
+            const coord = getRandomCoord();
+            try{
+                this.board.placeShip({startingCoord:coord, length: length, orient:orient});
+                return;
+            }
+            catch(e){
+               ;// in this case the error is useless other than
+               //as a signal to try again 
+            }
+        }
+        
+    }
+
     playerAttack(coord:coords){
         this.board.receiveAttack(coord);
     }
+
+
 }
 
 
